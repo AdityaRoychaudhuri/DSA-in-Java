1bcced2108176ff034f999116b9fa8c895f0e9bc
Aditya R <adiroychaudhuri@gmail.com>
2025-07-22 01:37:04 +0530

    Added solution for leetcode Q.141

---
+++ linkedListCycle.java
@@ -0,0 +1,39 @@
+/* Q.141 ==> https://leetcode.com/problems/linked-list-cycle/description */
+
+
+public class linkedListCycle {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next; // cycle here
+
+        linkedListCycle solution = new linkedListCycle();
+        System.out.println(solution.hasCycle(head));
+    }
+
+    boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
